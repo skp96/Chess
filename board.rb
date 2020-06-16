20feb2ca648ab3dc224b372fd81d80c5c9f3e57c
Sunny Patel <patel.sunny293@gmail.com>
2020-06-16 13:34:43 -0400

    added logic for checkmate

--- board.rb
+++ board.rb
@@ -3,9 +3,9 @@ require_relative 'pieces'
 class Board
   attr_reader :rows
 
-  def initialize
+  def initialize(fill_board = true)
     @sentinel = NullPiece.instance
-    start_board
+    start_board(fill_board)
   end
 
   def [](pos)
@@ -29,19 +29,10 @@ class Board
 
     raise "Don't try to cheat! Move your own piece." if color != piece.color
     raise "The piece does not move like that, please try again." if !piece.moves.include?(end_pos)
+    raise "This move will leave you in check" if !piece.valid_moves.include?(end_pos)
 
     move_piece!(color, start_pos, end_pos)
     
-  end
-
-  def move_piece!(color, start_pos, end_pos)
-    piece = self[start_pos]
-
-    self[start_pos] = sentinel
-    self[end_pos] = piece
-    piece.pos = end_pos
-
-    nil
   end
 
   def valid_pos?(pos)
@@ -57,42 +48,81 @@ class Board
   def empty?(pos)
     piece = self[pos]
 
-    if piece.class.name === 'NullPiece'
-      return true
-    else
-      false
+    piece.class.name == "NullPiece"
     end
+
+  end
+
+  def checkmate?(color)
+    return false unless in_check?(color)
+    pieces.select {|piece| piece.color == color}.all? do |p|
+      p.valid_moves.empty?
+    end
+  end
+
+  def in_check?(color)
+    king_pos = find_king.pos
+
+    pieces.any? {|piece| piece.color != color && piece.moves.include?(king_pos)}
+  end
+
+  def find_king(color)
+    king = pieces.find {|piece| piece.color == color && piece.class.name == 'King'}
+    king
+  end
+
+  def pieces
+    @rows.flatten.reject {|piece| piece.class.name == 'NullPiece'}
+  end
+
+  def dup
+    copy_board = Board.new(false)
+
+    pieces.each do |piece|
+      piece.class.new(piece.color, copy_board, piece.pos)
+    end
+    copy_board
+  end
+
+  def move_piece!(color, start_pos, end_pos)
+    piece = self[start_pos]
+
+    self[start_pos] = sentinel
+    self[end_pos] = piece
+    piece.pos = end_pos
+
+    nil
   end
 
   private
 
   attr_reader :sentinel
 
-  def start_board
+  def start_board(fill_board)
     @rows = Array.new(8) {Array.new(8, sentinel)}
+
     back_row = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
 
+    if fill_board == true 
+      rows.each_with_index do |row, row_idx|
+        if row_idx == 0 
+          8.times do |col_idx|
+            piece = back_row[col_idx]
+            piece.new('white', self, [row_idx, col_idx])
+          end
+        elsif row_idx == 1
+          8.times {|col_idx| Pawn.new('white', self, [row_idx, col_idx])}
+        end  
 
-    rows.each_with_index do |row, row_idx|
-      if row_idx == 0 
-        8.times do |col_idx|
-          piece = back_row[col_idx]
-          piece.new('white', self, [row_idx, col_idx])
-        end
-      elsif row_idx == 1
-        8.times {|col_idx| Pawn.new('white', self, [row_idx, col_idx])}
-      end  
-
-      if row_idx == 7 
-        8.times do |col_idx|
-          piece = back_row[col_idx]
-          piece.new('black', self, [row_idx, col_idx])
-        end
-      elsif row_idx == 6
-        8.times {|col_idx| Pawn.new('black', self, [row_idx, col_idx])}
-      end  
+        if row_idx == 7 
+          8.times do |col_idx|
+            piece = back_row[col_idx]
+            piece.new('black', self, [row_idx, col_idx])
+          end
+        elsif row_idx == 6
+          8.times {|col_idx| Pawn.new('black', self, [row_idx, col_idx])}
+        end  
+      end
     end
   end
-  
-  
 end
--- pieces/piece.rb
+++ pieces/piece.rb
@@ -19,5 +19,17 @@ class Piece
     false
   end
 
+  def valid_moves
+    moves.reject {|move| move_into_check?(move)}
+  end
+
+  private
+
+  def move_into_check?(end_pos)
+    copy_board = board.dup
+    copy_board.move_piece(color, pos, end_pos)
+    copy_board.in_check?(color)
+  end
+
 
 end
